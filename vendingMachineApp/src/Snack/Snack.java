a554870d05311f4588d9b51904d39ce5e46f3df0
bekah <elisaalvarez211@gmail.com>
2020-08-04 10:10:08 -0700

    almost done

--- vendingMachineApp/src/Snack/Snack.java
+++ vendingMachineApp/src/Snack/Snack.java
@@ -1,4 +1,4 @@
-package Snack;
+package vendingMachineApp.src.Snack;
 
 public class Snack
 {
@@ -9,7 +9,7 @@ public class Snack
  public double cost;
  public int vendingMachineId;
 
- 
+ //the snack obj
   public int getId()
   {
       return id;
@@ -26,4 +26,47 @@ public double getCost()
 {
   return cost;
 }
+public int getVendingMachineId()
+{
+  return vendingMachineId;
+}
+public int setId(int Id)
+{
+  return maxId=this.id;
+}
+public String setId(String name)
+{
+  return this.name=name;
+}
+public int setQuantitiy(int quantity)
+{
+  return this.quantity=quantity;
+}
+
+public int setVendingMachineId(int vendingMachineId)
+{
+  return this.vendingMachineId=vendingMachineId;
+}
+//add quantity
+
+public void addQuantity(int quantity)
+	{
+		this.quantity = this.quantity + quantity;
+  } 
+//buy snack
+
+public void buySnack(int quantity)
+	{
+		this.quantity = this.quantity - quantity;
+	}
+
+//total cost
+public double getTotalCost()
+{
+  double totalCost = this.cost * quantity;
+  return totalCost;
+}
+
+
+
 }
---
+++ vendingMachineApp/src/Snack/VendingMachine.java
@@ -0,0 +1,30 @@
+package vendingMachineApp.src.Snack;
+
+public class VendingMachine {
+    private static int maxId = 0;
+
+	private int id;
+	private String name;
+
+	
+	public Vendingmachine(String name)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+	  this.name = name;
+	}
+}
